4cb6e3ad6b3ed58eb29ff42382cdc0b16d04e82d
Anna Eliza <annaeliza.als@gmail.com>
2025-05-10 17:17:47 -0300

    Update test01.py

--- test01.py
+++ test01.py
@@ -5,4 +5,8 @@ response = requests.get('https://api.github.com/users/devAnnaEliza/repos')
 # check if the request was successful
 #print(response.status_code)
 
-print(response.headers['Content-Type'])
+#print(response.headers['Content-Type'])
+
+for item in range(len(response.json())):
+    print((response.json()[item]['name']))
+    
