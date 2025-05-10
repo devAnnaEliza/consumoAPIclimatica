f880ef44ee2aa4af481f1864578190149daf5f70
Anna Eliza <annaeliza.als@gmail.com>
2025-05-10 18:09:11 -0300

    Update test02.py

--- test02.py
+++ test02.py
@@ -1,8 +1,15 @@
 import requests
 
-#
 
-base_url = 'https://api.openweathermap.org/data/2.5/weather?'
-api_key = '4c47146bdd9e9fc59ae3f1d7a5bd0008'
-city = "Rio"
-complete_url = f'{base_url}&appid={api_key}&={city}'
+
+#response = requests.get(complete_url)
+#print(response.json)
+
+def get_wheather_API(city):
+    base_url = 'https://api.openweathermap.org/data/2.5/weather?'
+    api_key = '4c47146bdd9e9fc59ae3f1d7a5bd0008'
+    complete_url = f'{base_url}&appid={api_key}&q={city}'
+    response = requests.get(complete_url)
+    return response.json()
+
+def 
