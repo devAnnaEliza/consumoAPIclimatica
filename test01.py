45d440cfd71a83da272867f4cd6bc0abbf084902
Anna Eliza <annaeliza.als@gmail.com>
2025-05-10 17:09:35 -0300

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,6 +1,8 @@
 import requests
 
-response = requests.get('https://api.github.com/users/devAnnaEliza')
+response = requests.get('https://api.github.com/users/devAnnaEliza/repos')
 
 # check if the request was successful
-print(response.status_code)
+#print(response.status_code)
+
+print(response.headers['Content-Type'])
