40152b10d6bbcc45240bae07c94aecd706fc24ca
Anna Eliza <annaeliza.als@gmail.com>
2025-05-10 21:59:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from test02 import *
 if __name__ == "__main__":
     city = input("Digite o nome da cidade: ")
     print(f"Temperatura em {get_weather_temp(city)}°C")
-    print(f"Clima em {get_weather_main(city)}")
+    print(f"Clima: {get_weather_main(city)}")
